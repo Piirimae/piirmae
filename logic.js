ce998fc984e4ced336a4cd350f98db2af9e41a80
Piirimae <piirimaeinge@gmail.com>
2026-07-24 16:47:07 +0300

    Update logic.js
    Init start alla

--- logic.js
+++ logic.js
@@ -856,12 +856,13 @@ function uuendaVaateReziim() {
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("INIT START");
-    init();
+    
   // logic.js faili kõige lõppu lisada:
 
 // Impordime andmete laadimise funktsiooni kassatabel.js seest
 import { laeAndmedJaLukustus } from "./kassatabel.js";
 
 // Kui logic.js on tabeli genereerimise täielikult lõpetanud, kutsume selle välja:
-laeAndmedJaLukustus();  
+laeAndmedJaLukustus(); 
+    init();
 });
