e3f9d87063fede4a5e05e45ca243b56e40f6765d
Piirimae <piirimaeinge@gmail.com>
2026-08-02 15:04:34 +0300

    Update avaleht.js

--- avaleht.js
+++ avaleht.js
@@ -198,7 +198,7 @@ const tervitusElement = document.getElementById("avalehtDynaamilineTervitus");
 if (tervitusElement) {
     // OTSING & RAKENDAMINE: Parandatud võtmed ja stiili tükeldamine
     const dTekst = tekstideIndeks[`${esmaspaevaKuupaev}_AVALEHT_TERVITUS_TEKST`] || "Head isu !";
-    const dStiilRaw = tekstideIndeks[`${esmaspaevaKuupaev}_AVALEHT_TERVITUS_STIIL`] || "Brush Script MT;26px;#4a5568";
+    const dStiilRaw = tekstideIndeks[`${esmaspaevaKuupaev}_AVALEHT_TERVITUS_STIIL`] || "Comic Sans;26px;#c22e24";
     const [dFont, dSize, dColor] = dStiilRaw.split(";");
 
     tervitusElement.innerText = dTekst;
